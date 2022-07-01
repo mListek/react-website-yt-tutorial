df63472637161f834402d5a1dfb360c8b5e8edc1
Michał Listek <michal.listex@gmail.com>
2022-07-01 12:48:23 +0200

    small changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
+import { Button } from './Button'
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(prevClick => !prevClick);
-
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  useEffect(() => {
+    showButton();
+  }, [])
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -40,7 +56,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-
+          {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
         </div>
       </nav>
     </>
