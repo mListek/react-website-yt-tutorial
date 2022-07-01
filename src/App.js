50660a80e9d66d49a50736fb584e5a57c7341bd0
Michał Listek <michal.listex@gmail.com>
2022-07-01 16:51:19 +0200

    crated footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Services from './components/pages/Services'
 import Products from './components/pages/Products'
 import SignUp from './components/pages/SignUp'
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path='/products' element={<Products />} />
         <Route path='/sign-up' element={<SignUp />} /> 
       </Routes>
+      <Footer />
     </Router>
     </>
   );
